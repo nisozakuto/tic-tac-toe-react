4e601e539ad3c67ceb7a135d1f95d187af943a1d
nisozakuto <nisozakuto@gmail.com>
2020-11-25 11:20:46 -0500

    layout and texts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      is1Filled: false,
+      is2Filled: false,
+      is3Filled: false,
+      is4Filled: false,
+      is5Filled: false,
+      is6Filled: false,
+      is7Filled: false,
+      is8Filled: false,
+      is9Filled: false,
+    };
+  }
+
+  fillTheBox = (id) => {
+    console.log("clicked", id);
+  };
+
+  render() {
+    return (
+      <main>
+        <h1>Tic Tac Toe</h1>
+        <section className="scoreBoard">
+          <h2>
+            Player 1: <span>0</span>
+          </h2>
+          <h2>
+            Player 2: <span>0</span>
+          </h2>
+        </section>
+        <section>
+          <table>
+            <tr>
+              <th
+                id="1"
+                onClick={() => {
+                  this.fillTheBox(1);
+                }}
+              ></th>
+              <th id="2"></th>
+              <th id="3"></th>
+            </tr>
+            <tr>
+              <th id="4"></th>
+              <th id="5"></th>
+              <th id="6"></th>
+            </tr>
+            <tr>
+              <th id="7"></th>
+              <th id="8"></th>
+              <th id="9"></th>
+            </tr>
+          </table>
+        </section>
+      </main>
+    );
+  }
 }
-
-export default App;
