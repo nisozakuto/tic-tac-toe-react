dd001c648b9398d6a41001c37306fcbea719c723
nisozakuto <nisozakuto@gmail.com>
2020-11-26 21:35:09 -0500

    Game component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -7,6 +7,32 @@ const Game = () => {
     const [history,setHistory] = useState([Array(9).fill(null)])
     const [stepNumber,setStepNumber] = useState(0)
     const [xIsNext, setXisNext] = useState(true)
-    const winner = 
+    const winner = calculateWinner(history[stepNumber])
+    const xO = xIsNext ? "X" : "O"
 
+    const handleClick = (i)  => {
+        const historyPoint = history.slice(0, stepNumber + 1)
+        const current = historyPoint[stepNumber]
+        const squares =[...current]
+        if(winner || squares[i]) return
+        squares[i] = xO
+        setHistory([...historyPoint, squares])
+        setStepNumber(historyPoint.length)
+        setXisNext(!xIsNext)
+    }
+    return(
+        <>
+            <h1>Tic Tac Toe with React</h1>
+            <Board squares={history[stepNumber]} onClick={handleClick}/>
+            <div className="">
+                <div>
+                <h3>history</h3>
+                {renderMoves()}
+                </div>
+                <h3>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
+            </div>        
+        </>
+    )
 }   
+
+export default Game
