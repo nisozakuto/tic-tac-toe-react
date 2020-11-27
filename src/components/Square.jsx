147fc2437b31c033a5993e6fdd3321f5e1a126e9
nisozakuto <nisozakuto@gmail.com>
2020-11-26 21:16:17 -0500

    Square component

---
+++ src/components/Square.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Square=({value,onClick}) =>{
+    return(
+        <button onClick={onClick}>
+            {value}
+        </button>
+    )
+}
+export default Square
